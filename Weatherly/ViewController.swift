d5e54e672f210a2e8eb5d6c3a05c4d803ee98a6b
McKenzie Macdonald <kenziemacd@icloud.com>
2022-08-08 11:08:15 -0700

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by McKenzie Macdonald on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
