e83be5dd3ed93dada2c46713e1c28c1a45298600
McKenzie Macdonald <kenziemacd@icloud.com>
2022-08-08 12:33:40 -0700

    - Add UITextField protocol methods

--- Weatherly/ViewController.swift
+++ Weatherly/ViewController.swift
@@ -17,9 +17,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Textfield should report back to ViewController if the user interacts
+        searchTextField.delegate = self
+        
     }
 
-
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        if let userText = searchTextField.text {
+            print(userText)
+        }
+    }
+    
 }
 
+// MARK: TextFieldDelegate Methods
+
+extension ViewController: UITextFieldDelegate {
+    /// Says what to do when return button is pressed. Returns a Bool for if it should process that return.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        if let userText = searchTextField.text {
+            print(userText)
+        }
+        return true
+    }
+    
+    /// Runs when a user tries to deslect the textField
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != nil && textField.text != "" {
+            textField.placeholder = "Search"
+            return true
+        } else {
+            textField.placeholder = "Enter a city"
+            return false
+        }
+    }
+    
+    /// Clear textField when user is done editing
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+}
