9b189fb704fd8e5e72991bf2c6e822a156254bd8
McKenzie Macdonald <kenziemacd@icloud.com>
2022-08-08 11:55:56 -0700

    - Add outlets

--- Weatherly/ViewController.swift
+++ Weatherly/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+    @IBOutlet var tempUnitLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
